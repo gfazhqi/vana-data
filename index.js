840a2e89be6329e40bb630d6db0379fe5a5c0a85
zelix <61917052+ada682@users.noreply.github.com>
2024-09-19 15:26:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Vana {
     async main() {
         displayHeader();
         
-        const initData = 'query_id=AAGu72pYAAAAAK7valgj4vxX&user=%7B%22id%22%3A1483403182%2C%22first_name%22%3A%22%F0%9D%91%BE%F0%9D%92%90%F0%9D%92%8D%F0%9D%92%91%F0%9D%92%89%F0%9D%92%93%F0%9D%92%86%F0%9D%92%86%F0%9D%92%8F%20%7C%20Tomket%20Lovers%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22wolphreen7%22%2C%22language_code%22%3A%22id%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1726730419&hash=a37052ac78daa0d12c182499f7732e523ff8c184c7b9cfdb0ed3017551f510bd'; // Replace this with your actual query
+        const initData = 'your-query-here'; // Replace this with your actual query
 
         try {
             const playerData = await getPlayerData(initData);
